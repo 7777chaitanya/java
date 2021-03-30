994e4b0ba7ab1d92ab9afd07844196115595e441
Chaitanya7666 <chaitu.y25@gmail.com>
2021-03-30 09:22:50 +0530

    changes to array.java

--- src/com/hariSanjeev2/array.java
+++ src/com/hariSanjeev2/array.java
@@ -1,50 +1,23 @@
 package com.hariSanjeev2;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class array {
     public static void main(String[] args) {
-        int[] arr = {3,2,1,1,2};
-        int n = arr.length;
-        //sort the array
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-        //iterate till last element-1
-        for (int i = 0; i < n; i+=0) {
+        //declare an array
+        int[] a = {1, 2, 3, 2, 1, 2, 3};
 
-            //initialize count=1
-            int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            int flag = 0;
 
-            //iterate from i+1 till last element;j=i+1;j<n;j++
-
-                for (int j = i + 1; j < n; j++) {
-
-
-                    //IF(arr[i] == arr[j]
-                    if (arr[i] == arr[j]) {
-                        //count++
-                        count++;
-                    }
-                    //ELSE
-                    else {
-                        //sout(arr[i]
-                        System.out.println(arr[i]);
-                        count++;
-                        i += count;
-                        break;
-                    }
-                }
+            for (int j = 0; j < i; j++) {
+                if (a[i] == a[j])
+                    flag = 1;
             }
 
+            if (flag == 0)
+                System.out.print(a[i]+"   ");
         }
     }
+}
 
